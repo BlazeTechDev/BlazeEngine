b3b4fd9263f7df99e10f06b22fade3d003620eb9
x32dev <120539187+FlexTechDev@users.noreply.github.com>
2024-04-09 17:16:54 -0500

    Color Scheme struct

--- Blaze/ImGuiLayer.hpp
+++ Blaze/ImGuiLayer.hpp
@@ -11,6 +11,16 @@
 
 namespace Blaze
 {
+	struct ImGuiBlazeTheme
+	{
+		static constexpr ImVec4 BackgroundColor = ImVec4(0.16f, 0.16f, 0.17f, 1);
+		static constexpr ImVec4 DarkPrimary = ImVec4(0.15f, 0.15f, 0.15f, 1);
+		static constexpr ImVec4 DarkSecondary = ImVec4(0.11f, 0.11f, 0.11f, 1);
+		static constexpr ImVec4 LightPrimary = ImVec4(0.24f, 0.24f, 0.24f, 1);
+		static constexpr ImVec4 RedPrimary = ImVec4(0.94f, 0.24f, 0.24f, 255);
+		static constexpr ImVec4 RedSecondary = ImVec4(1, 0.33f, 0.33f, 1);
+	};
+
 	class BLAZE_API ImGuiLayer : public Layer
 	{
 	public:
